a1d0c03d1a544843a57432d4f89943cf4d2321f0
jopamo <p@1g4.org>
2024-09-14 23:10:56 -0400

    blake3: improve b3sum error handling

--- app-crypto/blake3/files/b3sum.c
+++ app-crypto/blake3/files/b3sum.c
@@ -9,9 +9,10 @@
 #include <errno.h>
 #include <pthread.h>
 #include <ctype.h>
+#include <getopt.h>
 
-#define CHUNK_SIZE 65536
 #define BLAKE3_OUT_LEN 32
+#define CHUNK_SIZE (BLAKE3_OUT_LEN * 1024)
 
 pthread_mutex_t output_mutex = PTHREAD_MUTEX_INITIALIZER;
 
@@ -51,13 +52,22 @@ static void print_version(void) {
   printf("b3sum version 1.0\n");
 }
 
+static void print_invalid_option(const char *option) {
+  fprintf(stderr, "b3sum: invalid option -- '%c'\n", option[1]);
+  fprintf(stderr, "Try 'b3sum --help' for more information.\n");
+}
+
+static void print_file_error(const char *filename) {
+  fprintf(stderr, "b3sum: %s: No such file or directory\n", filename);
+}
+
 static void *hash_file_thread(void *arg) {
   file_arg_t *file_arg = (file_arg_t *)arg;
   const char *filename = file_arg->filename;
 
   int fd = open(filename, O_RDONLY);
   if (fd < 0) {
-    perror("open");
+    print_file_error(filename);
     return NULL;
   }
 
@@ -87,8 +97,6 @@ static void *hash_file_thread(void *arg) {
 
   if (bytes_read < 0) {
     perror("read");
-    close(fd);
-    return NULL;
   }
 
   close(fd);
@@ -120,11 +128,19 @@ static void *hash_file_thread(void *arg) {
 static void process_files_concurrently(int argc, char **argv, file_arg_t *file_args) {
   int num_cpus = sysconf(_SC_NPROCESSORS_ONLN);
   pthread_t *threads = malloc(sizeof(pthread_t) * num_cpus);
+  if (threads == NULL) {
+    perror("malloc");
+    exit(EXIT_FAILURE);
+  }
+
   int thread_count = 0;
 
   for (int i = 1; i < argc; i++) {
     file_args[thread_count].filename = argv[i];
-    pthread_create(&threads[thread_count], NULL, hash_file_thread, &file_args[thread_count]);
+    if (pthread_create(&threads[thread_count], NULL, hash_file_thread, &file_args[thread_count]) != 0) {
+      perror("pthread_create");
+      exit(EXIT_FAILURE);
+    }
 
     thread_count++;
 
@@ -150,53 +166,87 @@ int main(int argc, char **argv) {
   int strict = 0;
   int warn = 0;
 
-  if (argc < 2) {
+  int opt;
+  while ((opt = getopt(argc, argv, "czw")) != -1) {
+    switch (opt) {
+      case 'c':
+        check = 1;
+        break;
+      case 'z':
+        zero = 1;
+        break;
+      case 'w':
+        warn = 1;
+        break;
+      default:
+        fprintf(stderr, "Invalid option: -%c\n", optopt);
+        print_help();
+        return EXIT_FAILURE;
+    }
+  }
+
+  static struct option long_options[] = {
+    {"check", no_argument, NULL, 'c'},
+    {"tag", no_argument, NULL, 0},
+    {"zero", no_argument, NULL, 'z'},
+    {"ignore-missing", no_argument, NULL, 0},
+    {"quiet", no_argument, NULL, 0},
+    {"status", no_argument, NULL, 0},
+    {"strict", no_argument, NULL, 0},
+    {"warn", no_argument, NULL, 'w'},
+    {"help", no_argument, NULL, 0},
+    {"version", no_argument, NULL, 0},
+    {0, 0, 0, 0}
+  };
+
+  int long_index = 0;
+  while ((opt = getopt_long(argc, argv, "czw", long_options, &long_index)) != -1) {
+    switch (opt) {
+      case 0:
+        if (strcmp(long_options[long_index].name, "tag") == 0) tag = 1;
+        if (strcmp(long_options[long_index].name, "ignore-missing") == 0) ignore_missing = 1;
+        if (strcmp(long_options[long_index].name, "quiet") == 0) quiet = 1;
+        if (strcmp(long_options[long_index].name, "status") == 0) status = 1;
+        if (strcmp(long_options[long_index].name, "strict") == 0) strict = 1;
+        break;
+      case 'h':
+        print_help();
+        return EXIT_SUCCESS;
+      case 'v':
+        print_version();
+        return EXIT_SUCCESS;
+      default:
+        print_invalid_option(argv[optind - 1]);
+        return EXIT_FAILURE;
+    }
+  }
+
+  if (optind >= argc) {
     print_help();
     return EXIT_FAILURE;
   }
 
-  for (int i = 1; i < argc; i++) {
-    if (strcmp(argv[i], "--check") == 0) {
-      check = 1;
-    } else if (strcmp(argv[i], "--tag") == 0) {
-      tag = 1;
-    } else if (strcmp(argv[i], "-z") == 0 || strcmp(argv[i], "--zero") == 0) {
-      zero = 1;
-    } else if (strcmp(argv[i], "--ignore-missing") == 0) {
-      ignore_missing = 1;
-    } else if (strcmp(argv[i], "--quiet") == 0) {
-      quiet = 1;
-    } else if (strcmp(argv[i], "--status") == 0) {
-      status = 1;
-    } else if (strcmp(argv[i], "--strict") == 0) {
-      strict = 1;
-    } else if (strcmp(argv[i], "-w") == 0 || strcmp(argv[i], "--warn") == 0) {
-      warn = 1;
-    } else if (strcmp(argv[i], "--help") == 0) {
-      print_help();
-      return EXIT_SUCCESS;
-    } else if (strcmp(argv[i], "--version") == 0) {
-      print_version();
-      return EXIT_SUCCESS;
-    }
+  file_args = malloc(sizeof(file_arg_t) * (argc - optind));
+  if (file_args == NULL) {
+    perror("malloc");
+    return EXIT_FAILURE;
   }
 
-  file_args = malloc(sizeof(file_arg_t) * (argc - 1));
-  for (int i = 0; i < argc - 1; i++) {
-    file_args[i].check = check;
-    file_args[i].tag = tag;
-    file_args[i].zero = zero;
-    file_args[i].ignore_missing = ignore_missing;
-    file_args[i].quiet = quiet;
-    file_args[i].status = status;
-    file_args[i].strict = strict;
-    file_args[i].warn = warn;
+  for (int i = optind; i < argc; i++) {
+    file_args[i - optind].filename = argv[i];
+    file_args[i - optind].check = check;
+    file_args[i - optind].tag = tag;
+    file_args[i - optind].zero = zero;
+    file_args[i - optind].ignore_missing = ignore_missing;
+    file_args[i - optind].quiet = quiet;
+    file_args[i - optind].status = status;
+    file_args[i - optind].strict = strict;
+    file_args[i - optind].warn = warn;
   }
 
   process_files_concurrently(argc, argv, file_args);
 
   free(file_args);
-  pthread_mutex_destroy(&output_mutex);
 
   return EXIT_SUCCESS;
 }
